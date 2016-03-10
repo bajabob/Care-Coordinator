fe14e2b1d42c9d17cffe1dcf7c371f9e74a1947d
Bob Timm <bajabob.tx@gmail.com>
2016-03-09 20:37:01 -0600

    Bootstrap calendar is live. Only took 4.5 hours :dizzy_face:

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,4 +8,14 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-# Rails.application.config.assets.precompile += %w( search.js )
+
+# for global styling
+Rails.application.config.assets.precompile += %w( jquery.min.js )
+Rails.application.config.assets.precompile += %w( bootstrap.min.js )
+Rails.application.config.assets.precompile += %w( bootstrap-theme.min.css )
+Rails.application.config.assets.precompile += %w( bootstrap.min.css )
+
+# for calendar: https://github.com/Serhioromano/bootstrap-calendar
+Rails.application.config.assets.precompile += %w( calendar.css )
+Rails.application.config.assets.precompile += %w( calendar.js )
+Rails.application.config.assets.precompile += %w( underscore.js )
