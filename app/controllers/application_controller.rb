cb3ce5b1b48fbc03fe1d0ab7eed3eb0bd230397a
Bob Timm <bajabob.tx@gmail.com>
2016-03-26 13:31:29 -0500

    #97 helper, current_user created to get current logged in user.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,16 +5,22 @@ class ApplicationController < ActionController::Base
 
   protected
 
+  # check if a user is currently logged in, used as a before_filter in controllers
   def authenticate_user!
-
     if(!session[:user_id].present?)
       redirect_to accounts_login_url
     end
-
   end
 
-  private
-
-  @current_user = nil
+  # HELPER: get the currently logged in user model obj
+  # in your controller, call `current_user` to get the current logged in user object
+  def current_user
+    if(session[:user_id].present?)
+      return User.where(:id => session[:user_id]).first
+    else
+      raise 'No user currently logged in, yet one is expected. Please make sure your controller is calling
+              `before_action :authenticate_user!` to check that a user is logged in.'
+    end
+  end
 
 end
