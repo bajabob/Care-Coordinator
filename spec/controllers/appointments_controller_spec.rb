2ca0e149d727dbfaed4408d8a88b3901614bd7d4
matpd <matpd@matpd.tech>
2016-03-28 14:31:18 -0500

    Rspec tests for delete, update, and create finished and working

--- spec/controllers/appointments_controller_spec.rb
+++ spec/controllers/appointments_controller_spec.rb
@@ -33,11 +33,15 @@ describe AppointmentsController do
      it 'should update an appointment' do
       @m=Itinerary.create :start => {"start(1i)" => 2016, "start(2i)" => 3, "start(3i)" => 2, "start(4i)" => 8, "start(5i)" => 30}, :description => "Get Blood", :id => "1000", :care_provider_id => "1"
       put :update, :id => "1000", :appointment => {"start(1i)" => 2016, "start(2i)" => 3, "start(3i)" => 2, "start(4i)" => 8, "start(5i)" => 30, "end(1i)" => 2016, "end(2i)" => 3, "end(3i)" => 2, "end(4i)" => 8, "end(5i)" => 30, :care_provider_id => "Dallas Health Group"}
-      flash[:notice].should =~ /updated/i
+      flash[:info].should =~ /updated/i
     end
     it 'should create an appointment' do
       post :create, :appointment => {"start(1i)" => 2016, "start(2i)" => 3, "start(3i)" => 2, "start(4i)" => 8, "start(5i)" => 30, "end(1i)" => 2016, "end(2i)" => 3, "end(3i)" => 2, "end(4i)" => 8, "end(5i)" => 30, :office_name => "Dallas Health Group"}
-      flash[:notice].should =~ /created/i
+      flash[:info].should =~ /created/i
+    end
+    it 'should delete an appointment' do
+       @m=Itinerary.create :description => "Get Blood", :id => "1000", :care_provider_id => "1"
+       delete :destroy, :id => 1000
     end
   end
   describe "convert date functions" do
